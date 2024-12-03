bce4ece7952ab83724837e50ed4f175a238e5418
一条老鱼 <hainasjoy@gmail.com>
2024-12-03 18:17:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -309,11 +309,11 @@ const BLOG = {
     'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/aplayer/1.10.1/APlayer.min.js',
   MUSIC_PLAYER_ORDER: process.env.NEXT_PUBLIC_MUSIC_PLAYER_ORDER || 'list', // 默认播放方式，顺序 list，随机 random
   MUSIC_PLAYER_AUDIO_LIST: [
-    // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/ https://music.163.com/song/media/outer/url?id=731419.mp3
+    // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/ 
     {
       name: '风を共に舞う気持ち',
       artist: 'Falcom Sound Team jdk',
-      url: 'https://podcasts.apple.com/cn/browse',
+      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
       cover:
         'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
     },
